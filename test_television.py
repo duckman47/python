d05366740404d572eb9f6aa71a5de66b30c3dacf
duckman47 <arsnyder514@gmail.com>
2026-04-10 21:28:21 -0500

    updated

---
+++ test_television.py
@@ -0,0 +1,50 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_init(self):
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+
+    def test_power(self):
+            self.assertEqual(True, False)
+            self.assertEqual(True, False)
+
+
+    def test_mute(self):
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+
+
+    def test_channel_up(self):
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+
+
+    def test_channel_down(self):
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+
+
+    def test_volume_up(self):
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+
+
+    def test_volume_down(self):
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+        self.assertEqual(True, False)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
